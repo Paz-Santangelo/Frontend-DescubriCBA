0b94b94e0b2af1f4ce942c2a771208a09d9732e4
Paz-Santangelo <paz_santangelo@hotmail.com>
2025-09-20 13:01:55 -0300

    Se ha creado la carpeta y los archivos jsx + css correspondientes al footer, y se ha instalado la dependencia de bootstrap icons.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import AppNavbar from './components/navbar/Navbar.jsx';
+import Footer from './components/footer/Footer.jsx';
 
 function App() {
   return (
-    <>
+    <div className="d-flex flex-column min-vh-100">
       <AppNavbar />
-    </>
+      <main className="flex-grow-1">
+        {/* Aca se van a agregar las rutas de las paginas */}
+      </main>
+      <Footer />
+    </div>
   );
 }
 
