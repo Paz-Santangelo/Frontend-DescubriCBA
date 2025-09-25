aed307136aab1d917a50211d412fd7e8cbc476b0
Paz-Santangelo <paz_santangelo@hotmail.com>
2025-09-25 16:36:24 -0300

    Se han agregado la foto de perfil con un menú desplegable en el navbar para simular un navbar con usuario logueado, así como los componentes MyProfile y DestinationsList para que puedan continuar trabajando mis compañeros.

---
+++ src/pages/destinationsList/DestinationsList.jsx
@@ -0,0 +1,11 @@
+import "./destinationsList.css";
+
+const destinationList = () => {
+  return (
+    <div className="destinations-container">
+      <h2>Seleccione un Destino</h2>
+    </div>
+  );
+};
+
+export default destinationList;
