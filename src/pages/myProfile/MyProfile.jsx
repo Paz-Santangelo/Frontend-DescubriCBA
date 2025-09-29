3e81a2378e81cdbcadfb7fcb28203fa6008f6c09
mroxanam <mroxanam20@gmail.com>
2025-09-29 12:06:38 -0300

    Realicé myprofile.jxs y myprofile.css

--- src/pages/myProfile/MyProfile.jsx
+++ src/pages/myProfile/MyProfile.jsx
@@ -1,11 +1,57 @@
 import './myprofile.css';
 
 const MyProfile = () => {
+
+  const user = {
+    name: "",
+    lastname: "",
+    email: "",
+    role: "USER",
+    image: null,
+  };
+
   return (
     <div className="profile-container">
-      <h2>Mi Perfil</h2>
+      <h2 className="profile-title">Mi Perfil</h2>
+
+      <div className="profile-image-container">
+        {user.image ? (
+          <img src={user.image} alt="Foto de perfil" className="profile-image" />
+        ) : (
+          <div className="profile-placeholder">Foto Perfil</div>
+        )}
+      </div>
+
+      {/* Caja con datos */}
+      <div className="profile-data-card">
+        <div className="profile-item">
+          <label>Nombre:</label>
+          <p>{user.name}</p>
+        </div>
+
+        <div className="profile-item">
+          <label>Apellido:</label>
+          <p>{user.lastname}</p>
+        </div>
+
+        <div className="profile-item">
+          <label>Email:</label>
+          <p>{user.email}</p>
+        </div>
+
+        <div className="profile-item">
+          <label>Rol:</label>
+          <p>{user.role}</p>
+        </div>
+      </div>
+
+      <div className="profile-buttons">
+        <button className="btn-delete">Eliminar</button>
+        <button className="btn-edit">Editar</button>
+      </div>
     </div>
   );
 };
 
 export default MyProfile;
+
